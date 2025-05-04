bc6bb09058af0dae7d4ed1cebdb47aa7012d7e0a
Fialkovska Mariia <mariyafialkovskaya@gmail.com>
2025-05-04 18:13:16 +0200

    Update SavingsBank.sol

--- SavingsBank.sol
+++ SavingsBank.sol
@@ -7,58 +7,46 @@ contract SavingsBank {
     event Deposited(address sender, uint256 amount);
     event Withdrawn(address receiver, uint256 amount);
 
-    // Adding constructor
+    struct Donation {
+        address sender;
+        uint256 amount;
+    }
+
+    Donation[] public donations;
+
     constructor() {
-        // Here - address of the account that deployed the contract
         owner = msg.sender;
     }
 
-    // Access control check
     modifier onlyOwner() {
-        // Error if caller is not the owner
         require(msg.sender == owner, "Only the owner can do this.");
         _;
-        // _; means "if everything is fine, continue executing the function"
     }
 
-    // Allows depositing funds into the contract
     function deposit() external payable {
-        // Checking if any funds were sent
         require(msg.value > 0, "You must send some Ether.");
-
-        // Emitting event to show that someone sent funds
+        donations.push(Donation(msg.sender, msg.value));
         emit Deposited(msg.sender, msg.value);
     }
 
-    // Allows the owner to withdraw funds
     function withdraw() external onlyOwner {
-        // Saving the current balance
         uint256 balance = address(this).balance;
-
-        // Checking if there are funds to withdraw
         require(balance > 0, "No funds to withdraw.");
-
-        // Sending all the funds to the contract owner
         payable(owner).transfer(balance);
-
-        // Emitting event that funds were withdrawn
         emit Withdrawn(owner, balance);
     }
 
-    // Returns the current contract balance
     function getBalance() external view returns (uint256) {
         return address(this).balance;
     }
 
-
-    // Request total number of donations
-    function getDonationCount() external view returns (uint256 count) {
-    return donations.length;
+    function getDonationCount() external view returns (uint256) {
+        return donations.length;
     }
 
-    // Receiving a specific donation by index
     function getDonation(uint256 index) external view returns (address sender, uint256 amount) {
-    require(index < donations.length, "Invalid index");
-    Donation memory d = donations[index];
-    return (d.sender, d.amount);
+        require(index < donations.length, "Invalid index");
+        Donation memory d = donations[index];
+        return (d.sender, d.amount);
     }
+}
